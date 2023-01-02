7c2ebae7ef5665fd2326e4d89284269a8c280006
Макс <maxtaba585@gmail.com>
2023-01-02 22:50:21 +0300

    121

--- main_menu.py
+++ main_menu.py
@@ -7,7 +7,7 @@ from PyQt5 import uic
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from to_lvl import To_lvl
+from in_game_window_lvl1 import Game_window
 
 
 class Main_Window(QMainWindow):
@@ -22,6 +22,22 @@ class Main_Window(QMainWindow):
 
     def open_new_game(self):
         shahid.show()
+        QMainWindow.close(self)
+
+
+class To_lvl(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui files/to_lvl.ui", self)
+        self.to_lvl1_button.clicked.connect(self.to_lvl1)
+        self.to_lvl2_button.clicked.connect(self.to_lvl2)
+
+    def to_lvl1(self):
+        alah.show()
+        QMainWindow.close(self)
+
+    def to_lvl2(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -29,4 +45,5 @@ if __name__ == '__main__':
     ex = Main_Window()
     ex.show()
     shahid = To_lvl()
+    alah = Game_window()
     sys.exit(app.exec_())
